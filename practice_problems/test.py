93fd7070091f4e01a59f4ce8bd714f9eb64dd1eb
JHpusd <justinhongdebate@gmail.com>
2024-07-01 18:18:21 -0700

    test commit

--- practice_problems/test.py
+++ practice_problems/test.py
@@ -3,7 +3,4 @@ from sklearn.linear_model import RidgeCV
 from sklearn.linear_model import LassoCV
 import numpy as np
 
-arr1 = np.array([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15]])
-
-idxs = [0, 1, 4]
-print(arr1[:,idxs])
+test = np.array([[1, 2], [3, 4], [5, 6]])
