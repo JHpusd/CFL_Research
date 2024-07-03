015fbadec549466cca732b6b6b5939fa9f4a4ce9
JHpusd <justinhongdebate@gmail.com>
2024-07-03 15:48:29 -0700

    went over day5, couldn'tload data

--- practice_problems/test.py
+++ practice_problems/test.py
@@ -2,5 +2,16 @@ from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import RidgeCV
 from sklearn.linear_model import LassoCV
 import numpy as np
+import pandas as pd
 
-test = np.array([[1, 2], [3, 4], [5, 6]])
+test = np.array(np.arange(36).reshape(6, 6))
+
+df = pd.DataFrame(data=test, columns=['a', 'b', 'c', 'd', 'e', 'f'])
+
+d1_idx = np.array([0, 2, 5])
+d2_idx = np.array([1, 3])
+
+x = test[:,d1_idx]
+y = test[:,d2_idx]
+
+regressor = LinearRegression().fit(x.reshape(-1, 1), y.reshape(-1, 1))
