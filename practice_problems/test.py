65d975a802cc249a0d2e3d988ade0ea1188b9971
jyhong <jyhong@caltech.edu>
2024-07-10 08:28:29 -0700

    finished day9 exercise 2

--- practice_problems/test.py
+++ practice_problems/test.py
@@ -2,13 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 
-def test_func(x,y):
-    return x+y, x*y
+test1 = np.array([1,2,3,4,5,6,7,8,9,10])
+test2 = np.array([1,2,3,4,5,6,7,8,9,10])
+test3 = np.array([1,2,3,4,5,6,7,8,9,10])
 
-def test_func2(x,y):
-    return x+y, x*y
-
-test = test_func(1,2)
-print(test)
-
-print(test_func2(*test))
+print(np.vstack([test1, test2, test3]))
